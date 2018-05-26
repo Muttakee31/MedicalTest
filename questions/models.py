44462d76bfcb41b6216a7517223b81e7f81c090f
Saad17 <muttakee9@gmail.com>
2018-05-26 22:56:27 +0600

    integrated

--- questions/models.py
+++ questions/models.py
@@ -16,6 +16,7 @@ class QuestionSet(models.Model):
 
 class ChapterQuestion(models.Model):
 
+    Medivar=models.IntegerField;
     SubName=models.CharField(max_length=50)
     SubID=models.CharField(max_length=50)
     CHapterNam=models.CharField(max_length=50)
@@ -33,29 +34,11 @@ class ChapterQuestion(models.Model):
         return self.SubName
 
 
-class VarsityQuestion(models.Model):
-
-    SubName=models.CharField(max_length=50)
-    SubID=models.CharField(max_length=50)
-    CHapterNam=models.CharField(max_length=50)
-    Question=models.TextField(max_length=1000)
-    Option1=models.CharField(max_length=50)
-    Option2=models.CharField(max_length=50)
-    Option3=models.CharField(max_length=50)
-    Option4=models.CharField(max_length=50)
-    CorrectAns=models.CharField(max_length=1)
-
-    class Meta:
-        db_table = 'VarsityQuestion'
-
-    def __str__(self):
-        return self.SubName
 
 
+class ExQuestion(models.Model):
 
-
-class ExMedicalQuestion(models.Model):
-
+    Medivar=models.IntegerField;
     QuestionId=models.ForeignKey(QuestionSet, on_delete=models.CASCADE)
     Question = models.TextField(max_length=1000)
     Option1 = models.CharField(max_length=50)
@@ -71,42 +54,15 @@ class ExMedicalQuestion(models.Model):
         return self.QuestionId
 
 
-class ExVarsityQuestion(models.Model):
-
-    QuestionId=models.ForeignKey(QuestionSet,on_delete=models.CASCADE)
-    Question = models.TextField(max_length=1000)
-    Option1 = models.CharField(max_length=50)
-    Option2 = models.CharField(max_length=50)
-    Option3 = models.CharField(max_length=50)
-    Option4 = models.CharField(max_length=50)
-    CorrectAns = models.CharField(max_length=1)
-
-    class Meta:
-        db_table = 'ExVarsityQuestion'
-
-    def __str__(self):
-        return self.QuestionId
-
-
-class ExDentalQuestion(models.Model):
-
-    QuestionId=models.ForeignKey(QuestionSet,on_delete=models.CASCADE)
-    Question = models.TextField(max_length=1000)
-    Option1 = models.CharField(max_length=50)
-    Option2 = models.CharField(max_length=50)
-    Option3 = models.CharField(max_length=50)
-    Option4 = models.CharField(max_length=50)
-    CorrectAns = models.CharField(max_length=1)
-
-    class Meta:
-        db_table = 'ExDentalQuestion'
-
-    def __str__(self):
-        return self.QuestionId
-
 class ExamHistory(models.Model):
     userid=models.ForeignKey(User, on_delete=models.CASCADE)
     Question_id =models.ForeignKey(QuestionSet,on_delete=models.CASCADE)
     TableName=models.CharField(max_length=15)
     marks=models.IntegerField(null=True)
     position=models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'ExamHistory'
+
+    def __str__(self):
+        return self.marks
