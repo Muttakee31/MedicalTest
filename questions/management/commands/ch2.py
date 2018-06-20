907955c803580dd7f78ba096d0806b557a918a2a
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2018-06-21 00:23:12 +0600

    Merge branch 'master' of https://github.com/Muttakee31/MedicalTest
    # view change
    #no redundant data entry possible e_history
    #database_command_custom_added
    #payment_sandbox_done_with
    #engl_script added
    #bio1_script added

---
+++ questions/management/commands/ch2.py
@@ -0,0 +1,45 @@
+import xlrd
+from django.core.management.base import BaseCommand, CommandError
+from questions.models import ChapterQuestion, QuestionSet
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        # try:
+        workbook = xlrd.open_workbook("C:/chem2.xlsx")
+        worksheet_names = workbook.sheet_names()
+
+        for i in range(len(worksheet_names)):
+            sheet = workbook.sheet_by_name(worksheet_names[i])
+            print(i)
+            print("FML")
+
+            for j in range(1, sheet.nrows):
+                qS = QuestionSet()
+                cQ = ChapterQuestion()
+                cQ.SubID = 4
+                cQ.SubName = "Ch2"
+                cQ.ChapterName = worksheet_names[i]
+                cQ.MVD = "M"
+                print(sheet.cell(j, 1).value)
+                cQ.Question = sheet.cell(j, 1).value
+                if ChapterQuestion.objects.filter(Question=cQ.Question).exists():
+                    continue
+                print(cQ.Question)
+                cQ.Option1 = sheet.cell(j, 2).value
+                print(cQ.Option1)
+                cQ.Option2 = sheet.cell(j, 3).value
+                cQ.Option3 = sheet.cell(j, 4).value
+                cQ.Option4 = sheet.cell(j, 5).value
+                if QuestionSet.objects.filter(QuestionName=worksheet_names[i]).exists():
+                    pass
+                else:
+                    qS = QuestionSet.objects.create(QuestionName=worksheet_names[i])
+                    qS.QuestionPrice = 5
+                qS = QuestionSet.objects.get(QuestionName=worksheet_names[i])
+                cQ.QuestionId = qS
+                cQ.CorrectAns = sheet.cell(j, 6).value
+                if cQ.Question or cQ.Option1:
+                    cQ.save()
+        # except Exception as e:
+        #     CommandError(repr(e))
